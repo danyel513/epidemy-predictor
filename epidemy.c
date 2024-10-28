8953ac0732725c5ef65670fa808831a9bfc20683
danyel513 <daniel.ben.cotoc@gmail.com>
2024-10-28 12:43:23 +0200

    Function start_simulation with 3 steps

--- epidemy.c
+++ epidemy.c
@@ -1,8 +1,224 @@
 #include "epidemy.h"
 
+// variable data
+int MAX_X_COORD, MAX_Y_COORD; // maximal coordinates given on the first row of the file
+int TOTAL_SIMULATION_TIME; // simulation duration -> 1st argv
+char INPUT_FILE_NAME[100] = ""; // input file name -> 2nd argv
+int THREAD_NUMBER; // number of threads used -> 3rd argv
 
-
-int main()
+// function: erroeHandler() -> prints appropiate error message and stops the program
+void errorHandler(void)
 {
-    return 0;
+    perror(strerror(errno)); // print the right error message having the "errno" cod
+    exit(EXIT_FAILURE);
+}
+
+// function: checkArguments() -> checks if arguments respect conditions
+void checkArguments(int argc, char *argv[])
+{
+    if (argc != 4) // ./exe TOTAL_SIMULATION_TIME  InputFileName ThreadNumber
+    {
+        printf("Invalid arguments! \n");
+        exit(EXIT_FAILURE);
+    }
+
+    // read and check for correct arguments
+
+    char *endptr;
+
+    TOTAL_SIMULATION_TIME = (int) strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || TOTAL_SIMULATION_TIME <= 0)
+    {
+        printf("Invalid TOTAL SIMULATION TIME! \n");
+        exit(EXIT_FAILURE);
+    }
+
+    THREAD_NUMBER = (int) strtol(argv[3], &endptr, 10);
+    if (*endptr != '\0' || THREAD_NUMBER <= 0)
+    {
+        printf("Invalid THREADS NUMBER! \n");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(INPUT_FILE_NAME, argv[2]);
+    if(strcmp(INPUT_FILE_NAME, "") == 0)
+    {
+        printf("Invalid INPUT FILE NAME! \n");
+        exit(EXIT_FAILURE);
+    }
+
+// debug purpose: test that the values were saved properly
+#ifdef DEBUG
+    printf("Saved values from arguments: %d %s %d \n", TOTAL_SIMULATION_TIME, INPUT_FILE_NAME, THREAD_NUMBER);
+#endif
+}
+
+// function: readData() -> reads and saves the data from the input file
+Person_t* readData(int *n) // n reperesent the size of the array, it needs to be saved in the parameter, initialy it can be 0 or NULL
+{
+    // open file
+    FILE *inputFile = fopen(INPUT_FILE_NAME, "r");
+    if (inputFile == NULL) errorHandler();
+
+    // read data:
+
+    if (fscanf(inputFile, "%d %d", &MAX_X_COORD, &MAX_Y_COORD) != 2) errorHandler();
+
+    if (fscanf(inputFile, "%d", n) != 1) errorHandler();
+
+// debug purpose only: see values MAX_X_COORD, MAX_Y_VALUE and the number of people in file
+#ifdef DEBUG
+    printf("Saved values from file: max_x = %d, max_y = %d, n = %d \n", MAX_X_COORD, MAX_Y_COORD, *n);
+#endif
+
+    Person_t* p = (Person_t*) malloc(sizeof(Person_t) * (*n));
+    if (p == NULL)
+    {
+        printf("Memory allocation error! Couldn't alocate enough memory for the array. \n");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i=0; i< (*n); i++)
+    {
+        int aux1, aux2; // used it for the enum content
+
+        if (fscanf(inputFile, "%ld %d %d %d %d %d", &p[i].personID, &p[i].x, &p[i].y, &aux1, &aux2, &p[i].amplitude) != 6)
+            errorHandler();
+
+        // setting other data
+        p[i].infectionCounter = 0;
+        p[i].currentStatus = (Status_t) aux1;
+        p[i].movementDirection = (Direction_t) aux2;
+
+        // checking the input data integrity
+        if(p[i].x > MAX_X_COORD) p[i].x = MAX_X_COORD;
+        if(p[i].y > MAX_Y_COORD) p[i].y = MAX_Y_COORD;
+        if(p[i].currentStatus > 1 || p[i].currentStatus < 0) p[i].currentStatus = 0;
+        if(p[i].movementDirection > 3 || p[i].movementDirection < 0) p[i].movementDirection = 0;
+        if((p[i].movementDirection == 0 || p[i].movementDirection == 1 ) && p[i].amplitude > MAX_Y_COORD) p[i].amplitude = MAX_Y_COORD;
+        if((p[i].movementDirection == 2 || p[i].movementDirection == 3 ) && p[i].amplitude > MAX_X_COORD) p[i].amplitude = MAX_X_COORD;
+
+        // setting the decrementing variable
+        p[i].time = p[i].currentStatus ? SUSCEPTIBLE_DURATION : INFECTED_DURATION;
+    }
+
+    // close file
+    if(fclose(inputFile) == -1)
+    {
+        perror(strerror(errno));
+    }
+
+    // returning the chunk of memory - free in the main program
+    return p;
+}
+
+// function: printPersonArray() -> checks the read data integrity
+void printPersonArray(Person_t* p, int n)
+{
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%ld - x: %d - y: %d - %s - %d - %d \n", p[i].personID, p[i].x, p[i].y, !p[i].currentStatus ? "infected" : "susceptible", p[i].movementDirection, p[i].amplitude);
+    }
+}
+
+// function: movePerson() -> moves the person with "amplitude" size in their own movement direction
+void movePerson(Person_t *p)
+{
+    switch (p->movementDirection) // judging by the moving direction and the current position we determine the next position
+    {
+        case N:
+            if(p->y + p->amplitude > MAX_Y_COORD)
+            {
+                p->movementDirection = S;
+                p->y -= p->amplitude;
+            }
+            else
+            {
+                p->y += p->amplitude;
+            }
+            break;
+
+        case S:
+            if(p->y - p->amplitude < 0)
+            {
+                p->movementDirection = N;
+                p->y += p->amplitude;
+            }
+            else
+            {
+                p->y -= p->amplitude;
+            }
+            break;
+
+        case E:
+            if(p->x + p->amplitude > MAX_X_COORD)
+            {
+                p->movementDirection = W;
+                p->x -= p->amplitude;
+            }
+            else
+            {
+                p->x += p->amplitude;
+            }
+            break;
+
+        case W:
+            if(p->x - p->amplitude < 0)
+            {
+                p->movementDirection = E;
+                p->x += p->amplitude;
+            }
+            else
+            {
+                p->y -= p->amplitude;
+            }
+            break;
+        default: break;
+    }
+}
+
+// function: computeFutureStatus() -> defines the future status of the every person after they moved around
+void computeFutureStatus(Person_t *p, const int n, const int index)
+{
+    // case 1: the person is either immune or infected and the interval of time is not over
+    if ((p[index].currentStatus == INFECTED || p[index].currentStatus == IMMUNE) && p[index].time > SUSCEPTIBLE_DURATION)
+    {
+        p[index].time -= p[index].amplitude;
+        p[index].futureStatus = p[index].currentStatus;
+        return;
+    }
+
+    // case 2: the person was infected and is about to get cured (immune)
+    if (p[index].currentStatus == INFECTED && p[index].time <= SUSCEPTIBLE_DURATION)
+    {
+        p[index].time = IMMUNE_DURATION;
+        p[index].futureStatus = IMMUNE;
+        return;
+    }
+
+    // case 3: the person is susceptible to contact the desease
+    for(int i = 0; i < n; i++)
+    {
+        if(p[index].x == p[i].x && p[index].y == p[i].y && p[i].currentStatus == INFECTED)
+        {
+            p[index].futureStatus = INFECTED;
+            p[index].time = INFECTED_DURATION;
+            return;
+        }
+    }
+
+    // case 4: the perse\on did not get in contact with any infected people
+    p[index].futureStatus = SUSCEPTIBLE;
+    p[index].time = SUSCEPTIBLE_DURATION;
+
+}
+
+// function: updateStatus() -> passes from the current status to the future status every person
+void updateStatus(Person_t *p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        p[i].currentStatus = p[i].futureStatus;
+    }
 }
--- epidemy.h
+++ epidemy.h
@@ -1,26 +1,32 @@
 #ifndef EPIDEMY_H
 #define EPIDEMY_H
 
+// defined debug purpose variables
+#define DEBUG
+
 // used libraries
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 // predefined data
-#define INFECTED_DURATION 2 // duration of the sickness
-#define IMMUNE_DURATION 2 // after this the persone can get infected again
+#define INFECTED_DURATION 14 // duration of the sickness
+#define IMMUNE_DURATION 30 // after this the persone can get infected again
+#define SUSCEPTIBLE_DURATION 0 // signals that a person is predisposed to get infected
 
 // variable data
-int MAX_X_COORD, MAX_Y_COORD; // maximal coordinates given on the first row of the file
-int TOTAL_SIMULATION_TIME; // simulation duration -> 1st argv
-char InputFileName[]; // input file name -> 2nd argv
-int ThreadNumber; // number of threads used -> 3rd argv
+extern int MAX_X_COORD, MAX_Y_COORD; // maximal coordinates given on the first row of the file
+extern int TOTAL_SIMULATION_TIME; // simulation duration -> 1st argv
+extern char INPUT_FILE_NAME[100]; // input file name -> 2nd argv
+extern int THREAD_NUMBER; // number of threads used -> 3rd argv
 
 // define STATUS enum type
 typedef enum Status
 {
     INFECTED,
     SUSCEPTIBLE,
-    IMUNE
+    IMMUNE
 } Status_t;
 
 // define DIRECTION emun type
@@ -41,12 +47,21 @@ typedef struct Person
     Status_t futureStatus; // health status after epidemy "step"
     Direction_t movementDirection; // movement pattern
     int amplitude; // movement steps -> smaller than the size of the area
-    int infectionCounter = 0;
+    int infectionCounter; // how many times the person got infected over the simulation
+    int time; // decremented every time the status does not change from imune or infected
 } Person_t;
 
 // serial functions
 
 // parallel functions
 
+// general use functions
+Person_t* readData(int *n); // reads data from the file -> returns the Person array and the array size (as parameter)
+void printPersonArray(Person_t* personArray, int numOfPersons); // prints array data
+void checkArguments(int argc, char *argv[]); // checks and saves args
+void errorHandler(void); // prints appropiate message for error
+void movePerson(Person_t *p); // moves a person with one unit
+void computeFutureStatus(Person_t *p, int n, int index); // finds the next status of every person status
+void updateStatus(Person_t *p, int n);
 
 #endif
